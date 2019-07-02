f193aef0fec09ef7891f00c6de6ed731407ecd72
Lyndon Panton <LyndonPanton@gmail.com>
2019-07-02 14:11:46 +0100

    Added click and keydown event listener to chevron
    Key down event if key is space bar or enter

--- js/app.js
+++ js/app.js
@@ -2,4 +2,19 @@
 
 window.onload = function(event) {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
+
+	function toggle(chevron) {
+		
+	}
+
+	let chevron = document.getElementById("chevron");
+	chevron.addEventListener("click", function(event) {
+		toggle(this);
+	});
+
+	chevron.addEventListener("keydown", function(event) {
+		if (event.keyCode === 13 || event.keyCode === 32) {
+			toggle(this);
+		}
+	});
 };
